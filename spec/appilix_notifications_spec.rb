d0f4b72743517830334c8178ab424d5404846fc2
RoaaAlqaisi <rowaalqaisi@gmail.com>
2025-01-23 13:55:50 +0200

    Tests with stubs

--- spec/appilix_notifications_spec.rb
+++ spec/appilix_notifications_spec.rb
@@ -3,17 +3,24 @@ require_relative '../lib/appilix_notifications'
 RSpec.describe AppilixNotifications do
   app_key = "key1"
   api_key = "apikey1"
+
   it 'sends a notification successfully using GET' do
+    allow(AppilixNotifications).to receive(:appilix_send_notifications)
+      .with(app_key, api_key, 'Hello', 'This is a test notification', nil, nil).and_return({ 'status' => 'true' })
+
     response = AppilixNotifications.appilix_send_notifications(
-      app_key, api_key, 'Hello', 'This is a test notification', nil, nil)
+      app_key, api_key, 'Hello', 'This is a test notification', nil, nil
+    )
 
     expect(response).to be_a(Hash)
     expect(response['status']).to eq('true')
   end
 
   it 'fetches registered user tokens successfully with a valid page' do
-    response = AppilixNotifications.appilix_get_registered_user(
-      app_key, api_key, 1)
+    allow(AppilixNotifications).to receive(:appilix_get_registered_user)
+      .with(app_key, api_key, 1).and_return({ 'tokens' => ['token1', 'token2'] })
+
+    response = AppilixNotifications.appilix_get_registered_user(app_key, api_key, 1)
 
     expect(response).to be_a(Hash)
     expect(response).to have_key('tokens')
