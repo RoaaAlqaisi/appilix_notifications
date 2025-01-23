06d4878c9363efb4f18fb57c2af5d062054524fa
RoaaAlqaisi <rowaalqaisi@gmail.com>
2025-01-23 10:23:41 +0200

    Fetch registered user tokens with page parameter

--- lib/appilix_notifications.rb
+++ lib/appilix_notifications.rb
@@ -9,6 +9,7 @@ require 'json'
 module AppilixNotifications
   BASE_URL = "https://appilix.com/api"
   PUSH_NOTIFICATION_URL = "push-notification"
+  TOKEN_LIST_URL = "push_notification_token_list.php"
 
   module_function
 
@@ -32,14 +33,39 @@ module AppilixNotifications
     handle_response(response)
   end
 
+  def appilix_registered_user_tokens(app_key, api_key, page)
+    raise ArgumentError, "Page parameter is mandatory" unless page
+
+    uri = URI("#{BASE_URL}/#{TOKEN_LIST_URL}")
+    uri.query = URI.encode_www_form(
+      'app_key' => app_key,
+      'api_key' => api_key,
+      'page' => page
+    )
+
+    request = Net::HTTP::Get.new(uri)
+
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+
+    handle_response(response)
+  end
+
   def handle_response(response)
     case response
     when Net::HTTPSuccess
       JSON.parse(response.body)
     else
-      { 'error' => response.message, 'code' => response.code, 'body' => JSON.parse(response.body) }
+      { 'error' => response.message, 'code' => response.code, 'body' => parse_body_safe(response.body) }
     end
   rescue JSON::ParserError
     { 'error' => 'Invalid JSON response', 'body' => response.body }
+  end
+
+  def parse_body_safe(body)
+    JSON.parse(body)
+  rescue JSON::ParserError
+    body
   end
 end
